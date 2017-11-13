ec4f08956502f1959419d4eb9098e7360821165c
maschad <chad.nehemiah94@gmail.com>
2017-11-13 05:28:29 -0500

    updated git ignore

---
+++ src/pages/sign-out/sign-out.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the SignOutPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-sign-out',
+  templateUrl: 'sign-out.html',
+})
+export class SignOutPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SignOutPage');
+  }
+
+}
