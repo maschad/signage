6a49355ca09371e94dbd8f238f2b6626e10c3d72
Chad Nehemiah <chad.nehemiah94@gmail.com>
2017-11-24 07:54:51 -0500

    Need to finish designing card for Waiver detail

---
+++ src/utilities/utilities.ts
@@ -0,0 +1,12 @@
+/**
+ * Pipe allowing for iteration over objects
+ */
+
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({ name: 'keys',  pure: false })
+export class KeysPipe implements PipeTransform {
+    transform(value: any, args: any[] = null): any {
+        return Object.keys(value)//.map(key => value[key]);
+    }
+}
